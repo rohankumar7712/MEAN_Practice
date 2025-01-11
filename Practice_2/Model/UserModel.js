4ae7b56e55e16593b4f16039b114441d83da0fa2
Rohankumar kanojiya <156400287+rohankumar7712@users.noreply.github.com>
2025-01-11 09:51:42 +0530

    Add files via upload

---
+++ Practice_2/Model/UserModel.js
@@ -0,0 +1,25 @@
+const mongoose=require('mongoose')
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        require:true
+    },
+    emailid:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        require:true,
+        length:8
+    },
+    createdAt: { 
+        type: Date, 
+        default: Date.now
+    }
+
+})
+
+module.exports=mongoose.model("user",userSchema);
